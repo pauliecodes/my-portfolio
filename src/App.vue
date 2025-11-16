b6c53825fb45a37f5d740117cd29429ac21dae99
paula <pau9ale@gmail.com>
2025-11-16 19:55:26 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-full flex items-center justify-center bg-gray-800">
+    <h1 class="text-24px font-bold text-green-400">
+      Hello UnoCSS!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
